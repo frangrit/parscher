16a9157dddc1a6b7916daad1801c89b56cdfc22a
Dano Williams <dan.o.williams@gmail.com>
2016-03-10 22:54:02 -0800

    added jquery transitions for nav

--- rhymepaysage.com/dev/js/rp-postload.js
+++ rhymepaysage.com/dev/js/rp-postload.js
@@ -12,13 +12,18 @@ $(function() {
 
 
 $(document).ready(function(){
+	if($("#container").size()>0){
+	    if (document.createStyleSheet){
+	        document.createStyleSheet('/css/rp-app.css');
+	    }
+	    else {
+	        $("head").append($("<link rel='stylesheet' href='/css/rp-app.css' type='text/css' media='screen' />"));
+	    }
+	}
 
-if($("#container").size()>0){
-        if (document.createStyleSheet){
-            document.createStyleSheet('/css/rp-app.css');
-        }
-        else {
-            $("head").append($("<link rel='stylesheet' href='/css/rp-app.css' type='text/css' media='screen' />"));
-        }
-    }
+	$('#nav-secondary li a').on("click", function( event ){
+       event.preventDefault();
+	    thisBodyClass = $(this).attr('id');
+	    $('body').removeClass().addClass( thisBodyClass ); 
+    });
 });
